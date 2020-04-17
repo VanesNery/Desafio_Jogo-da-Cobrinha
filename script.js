ba0234ed13892aa0e335aa1bd0674f6a646ec035
vanesnery <nery_van@hotmail.com>
2020-04-16 23:10:16 -0300

    criado function para o jogo qnd a cobra se choca

--- script.js
+++ script.js
@@ -54,6 +54,13 @@ function initGame(){
         snake[0].y = 16 * box;
     }
 
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert("Game Over :[");
+        }
+    }
+
     createBG();
     createSnake();
     drawFood();
